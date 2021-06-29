0567dbf4e2af9cd6849320b74a2b69e712cee77e
GBstallman <nduwimanaguymich@gmail.com>
2021-06-29 06:03:06 -0700

    created UbwuzureBushaFragment and UbwuzureKeraFragment

---
+++ app/src/main/java/com/gbstallman/bibiliya_nyeranda/Fragments/UbwuzureBushaFragment.java
@@ -0,0 +1,23 @@
+package com.gbstallman.bibiliya_nyeranda.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.gbstallman.bibiliya_nyeranda.R;
+
+public class UbwuzureBushaFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_ubwuzurebusha, container, false);
+
+        return view;
+    }
+}
